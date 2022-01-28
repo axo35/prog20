1b4db15fc1f575a2649d9b27acec82f923958ac8
alexander.storrank <71625754+axo35@users.noreply.github.com>
2022-01-28 08:36:41 +0200

    init

---
+++ prog20/public/app.js
@@ -0,0 +1,9 @@
+function showName(name){
+    const root = document.createElement('div')
+    const text = document.createElement('h2')
+    text.innerHTML = name + ' hej!'
+    root.appendChild(text)
+    document.body.appendChild(root)
+}
+
+showName('alexander')
