21ce0218e23db5cb821f3dbb324957440fd29f4a
Zer0cheros <christian.wiss@optimaedu.fi>
2022-02-01 14:42:01 +0200

    init

--- prog20/server.js
+++ prog20/server.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const server = express()
+const mongoose = require('mongoose')
 server.use(express.static('public'))
+mongoose.connect('här lägger du din connection-string')
+const db = mongoose.connection
+db.on('open', (err)=>{
+    if(err)throw err
+    console.log('connected to database')
+})
+
+// ni behöver skapa en mongoose schema för att det ska kunna spara
+
+//
+
+server.post('/register', (req, res)=>{
+    const {name, email, password} = req.body
+    const info = new User({
+        name: name,
+        email:email,
+        password: password
+    })
+    info.save()
+
+})
 server.listen(process.env.PORT || 5000)
  
